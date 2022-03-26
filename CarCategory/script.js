2a1f857b11d6e617098fa7aed31ed7144a4628ad
Zhi Yao <zhiyao0223@gmail.com>
2022-03-26 14:56:04 +0800

    Complete car category page

--- CarCategory/script.js
+++ CarCategory/script.js
@@ -7,3 +7,18 @@ function mouseOutToggle() {
     var dropDownContent = document.getElementById('buy_content');
     dropDownContent.style.display = "none";
 }
+
+
+// Close Brand Popup Box
+function verification(option) {
+    var brandBox = document.getElementById("brand");
+
+    if (option == "open") {
+        brandBox.style.visibility = "visible";
+        brandBox.style.opacity = "1";
+    }
+    else if (option == "close") {
+        brandBox.style.visibility = "hidden";
+        brandBox.style.opacity = "0";
+    }
+} 
