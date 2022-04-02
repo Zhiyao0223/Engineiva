3e569770f1b11f6c0b056303dc04696f359c293e
nelo1302 <100836117+nelo1302@users.noreply.github.com>
2022-04-02 23:04:45 +0800

    payment

--- Payment/checkout.php
+++ Payment/checkout.php
@@ -1,10 +1,10 @@
 <?php
-// include('conn.php');
+include('conn.php');
 
-//     $carID = $_POST['carID'];
-//     $car = "SELECT * FROM car WHERE carID='$carID'";
-//     $record = mysqli_query($con,$car);
-//     $row = mysqli_fetch_array($record);
+    $carID = $_POST['carID'];
+    $car = "SELECT * FROM car WHERE carID='$carID'";
+    $record = mysqli_query($con,$car);
+    $row = mysqli_fetch_array($record);
 
 ?>
 
@@ -16,21 +16,19 @@
 <link rel="stylesheet" href="checkout.css">
 </head>
 <script>
-  function checkRates() {
-    var input = document.getElementsByName("cvv")[].innerHTML;
-    alert(input.length);
-    if (input.length > 3) {
-        document.getElementsByName("cvv").value = input.slice(0,3);
-    }
+function myFunction() {
+  // Get the value of the input field with id="numb"
+  let x = document.getElementsByName("cvv")[0].value;
 
-    // Check for zero rates
-    if (input.length != 3) {
-            alert("Error!");
-            document.getElementsByName("cvv").value = "";
-            exit(0);
-    }
-
+  let text;
+  if (isNaN(x) || x < 100 || x > 999) {
+    alert("CVV Error!");
+    document.getElementsByName("cvv")[0].value = "";
+    return;
+  } 
+  document.getElementsByName("cvv")[0].value = x;
 }
+
 </script>
 <body>
 <!DOCTYPE html>
@@ -85,28 +83,95 @@
             <div class="row">
               <div class="col-50">
                 <label for="expmonth">Exp Month</label>
-                <input type="number" id="partitioned" name="expmonth" placeholder="ExpMonth" required>
+                <input type="number" min="01" max="12" id="partitioned" name="expmonth" placeholder="01-12" required>
               </div>
               <div class="col-50">
                 <label for="expyear">Exp Year</label>
-                <input type="number" id="partitioned" name="expyear" placeholder="ExpYear" required>
+                <input type="number" min="22" max="27" id="partitioned" name="expyear" placeholder="22-27" required>
               </div>
             </div>
             <div class="row">
                 <label for="cvv">CVV</label>
-                <input type="number" id="partitioned" name="cvv" placeholder="CVV" onblur="checkRates()" required>
+                <input type="number" id="partitioned" name="cvv" placeholder="CVV" onblur="myFunction()" required>
             </div>
           </div>
       </form>
     </div>
   </div>
 
-  <?php
-       
-    ?>
     <form method="post">
         <center><input type="submit" class="btn" name="submitBtn" value="Checkout"/></center>
     </form>
- 
+
 </div>
 </body>
+<?php
+    if (isset($_POST['submitPromo'])){        //check whether promocode exists
+      $SQL = "SELECT * FROM promocode";
+      $record = mysqli_query($con,$SQL);
+      $promo = $_POST['promocode'];
+      while($rows = mysqli_fetch_array($record)){
+          if ($promo == $rows['promocode']){
+              $true = "<script>alert('Valid Promocode!!')</script>";
+              echo $true;
+
+              // Replace discount box with promocode discount
+              $offer = $rows['offer'];
+              $script = "<script>
+                            var discountBox = document.getElementById('discount').innerHTML;
+                            document.getElementById('discount').innerHTML = '$offer';
+                            document.getElementById('submitPromoCode').style.display = 'none';
+                            document.getElementById('promoInput').value = '$promo';
+
+                            // Change Attribute of input box
+                            document.getElementById('promoInput').readOnly = true;
+                            document.getElementById('promoInput').style.border = 'none';
+                            document.getElementById('promoInput').style.outline = 'none';
+                            document.getElementById('promoInput').style.textAlign = 'right';
+                            document.getElementById('promoInput').style.color = 'grey';
+                          </script>";
+              echo $script;
+              $total= $secure-$offer;
+              
+              $scripttotal="<script>
+                        document.getElementById('total').innerHTML = '$total';
+              </script>";
+              echo $scripttotal;
+          }
+          else {
+              $false = "<script>alert('Invalid Promocode!!')</script>";
+              echo $false;
+          }
+      }
+
+  }
+?>
+
+<?php
+  if (isset($_POST['submitBtn'])) {
+	include("conn.php");
+    
+  $custID;//session
+  $date = date('Y-m-d'); //set current date and time
+	$sql1="INSERT INTO cust_buy (carID, custID, date, secureFee, paymentMethod) 
+        VALUES ('$carID','?session?','$date','$total','Credit Card')";
+    
+    $sql = "UPDATE car SET 
+    sellStatus = 'Sold' 
+    WHERE carID='$carID'";
+    
+
+
+	if (!mysqli_query($con,$sql)){
+		die('Error: ' . mysqli_error($con));
+	}
+	else {
+    mysqli_query($con,$sql1);
+		echo '<script>alert("Payment Successful!");
+    
+    </script>';
+	}
+
+	mysqli_close($con);
+ }
+?>
