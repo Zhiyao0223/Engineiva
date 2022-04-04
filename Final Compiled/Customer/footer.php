caa73139ca0a41624b1b35a2453cef8257bc157d
Zhi Yao <zhiyao0223@gmail.com>
2022-04-04 14:38:27 +0800

    .

--- Final Compiled/Customer/footer.php
+++ Final Compiled/Customer/footer.php
@@ -1,8 +1,6 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Footer</title>
-
     <link rel="stylesheet", href="footer.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 </head>
