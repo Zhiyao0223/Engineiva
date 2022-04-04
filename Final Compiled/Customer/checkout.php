ac9b005bfd352f849761476714c7a27ba2e775fd
Zhi Yao <zhiyao0223@gmail.com>
2022-04-04 10:57:01 +0800

    .

--- Final Compiled/Customer/checkout.php
+++ Final Compiled/Customer/checkout.php
@@ -7,8 +7,15 @@ include('conn.php');
                 window.location.href='userlogin.php';
         </script>";
   }
+  if (isset($_POST['carID'])) {
+    $carID = $_POST['carID'];
+  }
+  else {
+    echo "<script>alert('Please login to proceed')
+              window.location.href='userlogin.php';
+          </script>";
+  }
 
-  $carID = $_POST['carID'];
   $car = "SELECT * FROM car WHERE carID='$carID'";
   $record = mysqli_query($con,$car);
   $row = mysqli_fetch_array($record);
@@ -30,90 +37,90 @@ include('conn.php');
     <script src="script.js"></script>
 </head>
 <body>
-<div class="col-25">
-    <div class="container">
-      <h1>Checkout
-        <span class="price" style="color:black">
-          <i class="fa fa-shopping-cart"></i>
-        </span>
-      </h1>
-      <p>Brand<span class="price"><?php echo $row['brand']?></span></p>
-      <p>Model<span class="price"><?php echo $row['model']?></span></p>
-      <p>Price<span class="price">RM<?php echo $row['price'] ?></span></p>
-      <p>Secure 1% <span class="price">RM<?php $secure = ($row['price']*0.01); echo $secure;?></span></p>
-      <form action="checkout.php" method="post" action='#'>
-        <p>Promo Code <span class="price"><input name="promocode" id='promoInput' style="width:70px">&nbsp<button type="submit" id='submitPromoCode' name="submitPromo">Submit</button></span></p>
-        <input type='text' name='carID' value='<?php echo $carID; ?>' hidden>
-      </form>
-      <p>Discount <span class="price" id='discount'></span></p>
-      <hr>
-      <p>Total <span class="price" id='total'>RM<?php echo $secure;?></span></p>
-    </div>
-  </div>
-</div>
-
-<form action="#" method='post'>
-  <div class="row">
-    <div class="col-75">
-      <div class="container2">
-
-          <h2>Please fill in below information.</h2><br>
-          <div class="row">
-          <div class="col-50">
-            <h3>Billing Address</h3>
-            <label for="fname">Full Name</label>
-            <input type="text" id="partitioned" name="contact_name" required>
-            <label for="email">Email</label>
-            <input type="text" id="partitioned" name="contact_email" required>
-            <label for="adr">Address</label>
-            <input type="text" id="partitioned" name="contact_address" required>
-            <label for="city">City</label>
-            <input type="text" id="partitioned" name="contact_city" required>
-
-            <div class="row">
-              <div class="col-50">
-                <label for="state">State</label>
-                <input type="text" id="partitioned" name="contact_state" required>
-              </div>
-              <div class="col-50">
-                <label for="zip">Zip</label>
-                <input type="text" id="partitioned" name="contact_zip" required>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-50">
-            <h3>Payment</h3>
-            <label for="cname">Name on Card</label>
-            <input type="text" id="partitioned" name="cardname" required>
-            <label for="ccnum">Card number</label>
-            <input type="text" id="partitioned" name="cardnumber" required>
+  <div class='main-container'>
+    <div class="col-25">
+        <div class="container">
+          <h1>Checkout
+            <span class="price" style="color:black">
+              <i class="fa fa-shopping-cart"></i>
+            </span>
+          </h1>
+          <p>Brand<span class="price"><?php echo $row['brand']?></span></p>
+          <p>Model<span class="price"><?php echo $row['model']?></span></p>
+          <p>Price<span class="price">RM<?php echo $row['price'] ?></span></p>
+          <p>Secure 1% <span class="price">RM<?php $secure = ($row['price']*0.01); echo $secure;?></span></p>
+          <form action="checkout.php" method="post" action='#'>
+            <p>Promo Code <span class="price"><input name="promocode" id='promoInput' style="width:70px">&nbsp<button type="submit" id='submitPromoCode' name="submitPromo">Submit</button></span></p>
             <input type='text' name='carID' value='<?php echo $carID; ?>' hidden>
-
-            <div class="row">
-              <div class="col-50">
-                <label for="expmonth">Exp Month</label>
-                <input type="text" id="partitioned" name="expmonth" required>
-              </div>
-              <div class="col-50">
-                <label for="expyear">Exp Year</label>
-                <input type="text" id="partitioned" name="expyear" required>
-              </div>
-            </div>
-            <div class="row">
-                <label for="cvv">CVV</label>
-                <input type="text" id="partitioned" name="cvv" required>
-            </div>
-          </div>
-          <div class='submit-section'>
-            <input type="submit" class="btn submitBtn" name="submitBtn" value="Checkout"/></center>
+          </form>
+          <p>Discount <span class="price" id='discount'></span></p>
+          <hr>
+          <p>Total <span class="price" id='total'>RM<?php echo $secure;?></span></p>
         </div>
       </div>
-  </div>
+    </div>
 
-</form>
- 
-</div>
+    <form action="#" method='post'>
+      <div class="row">
+        <div class="col-75">
+          <div class="container2">
+
+              <h2>Please fill in below information.</h2><br>
+              <div class="row">
+              <div class="col-50">
+                <h3>Billing Address</h3>
+                <label for="fname">Full Name</label>
+                <input type="text" id="partitioned" name="contact_name" required>
+                <label for="email">Email</label>
+                <input type="text" id="partitioned" name="contact_email" required>
+                <label for="adr">Address</label>
+                <input type="text" id="partitioned" name="contact_address" required>
+                <label for="city">City</label>
+                <input type="text" id="partitioned" name="contact_city" required>
+
+                <div class="row">
+                  <div class="col-50">
+                    <label for="state">State</label>
+                    <input type="text" id="partitioned" name="contact_state" required>
+                  </div>
+                  <div class="col-50">
+                    <label for="zip">Zip</label>
+                    <input type="text" id="partitioned" name="contact_zip" required>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-50">
+                <h3>Payment</h3>
+                <label for="cname">Name on Card</label>
+                <input type="text" id="partitioned" name="cardname" required>
+                <label for="ccnum">Card number</label>
+                <input type="text" id="partitioned" name="cardnumber" required>
+                <input type='text' name='carID' value='<?php echo $carID; ?>' hidden>
+
+                <div class="row">
+                  <div class="col-50">
+                    <label for="expmonth">Exp Month</label>
+                    <input type="text" id="partitioned" name="expmonth" required>
+                  </div>
+                  <div class="col-50">
+                    <label for="expyear">Exp Year</label>
+                    <input type="text" id="partitioned" name="expyear" required>
+                  </div>
+                </div>
+                <div class="row">
+                    <label for="cvv">CVV</label>
+                    <input type="text" id="partitioned" name="cvv" required>
+                </div>
+              </div>
+              <div class='submit-section'>
+                <input type="submit" class="btn submitBtn" name="submitBtn" value="Checkout"/></center>
+            </div>
+          </div>
+      </div>
+
+    </form>
+  </div>
 </body>
 <?php
     include("conn.php");
