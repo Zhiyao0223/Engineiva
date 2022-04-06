45e043b76b212db1b291df3462c2c7dabced473f
nelo1302 <100836117+nelo1302@users.noreply.github.com>
2022-04-06 11:41:51 +0800

    promo and checkout

--- Final Compiled/Admin/ad-promotion-detail.php
+++ Final Compiled/Admin/ad-promotion-detail.php
@@ -20,13 +20,13 @@
             <li><a href="ad_view_appointment.php">View Appointment</a></li>
             <li><a href="ad-promotion-detail.php" class="active">Promotion Detail </a></li>
             <li><a href="support-ticket.php">Support Ticket</a></li>
+            <li><a href="ad_order_history.php">Order History</a></li>
+            <li><a href='AdminReport.php'>Report Generation</a></li>
         </ul>
     </div>
 
     <div class="content-container">
         <h2>Promotion Details</h2>
-
-
         <table class="car-table">
             
             <tr class="table-title">
@@ -52,7 +52,8 @@
                                             <img src='imgAdmin/edit.png'>
                                         </a>
                                     </button>
-                                    <button class='deletebtn' name='delete-img' onclick=\"deletePromo('" .$row['id'] ."')\">
+                                    <button class='editbtn'>
+                                        <a href='promo-remove.php?id=" .$row['id']."'>
                                         <img src='imgAdmin/bin.png'>
                                     </button>
                                 </td>;
@@ -64,13 +65,6 @@
          <!-- End of car table -->
     </div>
     <!-- End of main content -->
-    <script> 
-        function deletePromo(promo){
-            if (confirm("Are you sure want to permenently delete the promotion?\n *All other record that consist this promocode will be deleted")){
-                window.location.href="promo-remove.php?promocode="+promocode;
-            }
-        }
-    </script> 
     
 </body>
 </html>
---
+++ Final Compiled/Admin/promo-remove.php
@@ -0,0 +1,41 @@
+<?php 
+        include('session.php');
+?>
+
+
+<?php
+
+    include("conn.php");
+
+    //$_GET[‘id’] —Get the integer value from id parameter in URL. 
+    //intval() will returns the integer value of a variable
+    $id=$_GET['id'];
+
+
+    $result=mysqli_query($con,"DELETE FROM promocode WHERE id=$id");
+
+    mysqli_close($con);//close database connection
+
+    header('Location: ad-promotion-detail.php'); //redirect the page to view.phppage
+
+
+
+
+  // If the SQL code failed to execute     
+  if (!mysqli_query($con,$promo)) {
+    // Close engineiva database connection  
+    mysqli_close($con);
+    // Echo delete failed and direct back to modify and remove page
+    echo '<script>alert("Promotion Delete Failed!"); window.location.href="ad-promotion-detail.php";</script>;';
+  }
+  // Else if the SQL code successfully excuted
+  else {
+    // Close engineiva database connection  
+    mysqli_close($con);
+    // Echo car deleted and direct back to modify and remove page
+    echo '<script>alert("Promotion Deleted!");
+    window.location.href= "ad-promotion-detail.php";
+    </script>';
+  }
+    
+?>
